d7eb4abde843f6253927a0660a0ed3b2d980c2f0
Ismail Cetin <isocetin1907@gmail.com>
2022-04-08 13:43:38 +0200

    Added back to top button in pokemon.blade

--- resources/views/all.blade.php
+++ resources/views/all.blade.php
@@ -57,23 +57,22 @@
 
             </div>
         </div>
-        <div id="buttonmore" class="mb-32 mt-10 mx-auto">
+        <div id="buttonmore" class="mb-32 mt-16  mx-auto">
             <div class="grid gap-8 items-start justify-center">
                 <div class="relative group">
-                    <div class="absolute -inset-0.5 bg-gradient-to-r from-blue-400 to-yellow-400  rounded-lg blur opacity-75"></div>
+                    <div class="absolute -inset-0.5 bg-gradient-to-r from-blue-500 to-blue-500  rounded-lg blur opacity-75"></div>
                     <button id="more" class="relative px-7 py-4 bg-white rounded-lg leading-none flex items-center divide-x divide-gray-600">
                         <span class="text-black">Load more</span>
                     </button>
                 </div>
             </div>
         </div>
-        <!-- Back to top button -->
+
         <button type="button" data-mdb-ripple="true" data-mdb-ripple-color="light" class="inline-block p-3 bg-white text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-gray-300  hover:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out hidden bottom-5 right-5 fixed" id="btn-back-to-top">
             <svg aria-hidden="true" focusable="false" data-prefix="fas" class="w-4 h-4" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                 <path fill="black" d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z"></path>
             </svg>
         </button>
-        
     </div>
     <script>
         function randomIntFromInterval(min, max) { // min and max included 
